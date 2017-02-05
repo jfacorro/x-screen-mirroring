58b8e9b416466697203d2ec362b6df1a72fc700e
Juan Facorro <juan.facorro@gmail.com>
2017-02-05 15:52:33 +0100

    Set options for fast encoding

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-FRAMERATE=1
+FRAMERATE=5
+# RESOLUTION=640x480
 RESOLUTION=1024x768
 INPUT="-f x11grab -r $FRAMERATE -s $RESOLUTION -i :0.0"
 FEED_URL=http://127.0.0.1:8090/feed-rpi.ffm
-# AUDIO=-i anullsrc
-# AUDIO=-f lavfi -i aevalsrc=0 -c:a libmp3lame
+PROBESIZE="-probesize 10M -threads 2"
 # ffmpeg -f x11grab -s 1024x768 -i :0.0 -c:v mpeg4 -f mp4 -f lavfi -i aevalsrc=0 -c:a libmp3lame $FEED_URL
-ffmpeg -probesize 10M $INPUT $AUDIO $FEED_URL
+ffmpeg $PROBESIZE $INPUT $FEED_URL
