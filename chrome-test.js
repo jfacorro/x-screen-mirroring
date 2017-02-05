4bb78f5218294df8c95d529eb7982302c95d2c31
Juan Facorro <juan.facorro@gmail.com>
2017-02-05 13:44:49 +0100

    Trying with webm

--- chrome-test.js
+++ chrome-test.js
@@ -2,44 +2,19 @@ var ChromecastAPI = require('chromecast-api')
 
 var browser = new ChromecastAPI.Browser()
 
+var urlMedia = { url : 'http://192.168.0.11:8000/screen.png'
+               , contentType: 'image/png'
+               }
+
 var update = function(device) {
-    
-}
-
-browser.on('deviceOn', function (device) {
-    // var urlMedia = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4';
-    // var urlMedia = 'http://192.168.0.11:8000/out.png';
-    // var urlMedia = 'http://192.168.0.11:8888';
-    //    var urlMedia = { url : 'https://jpeg.org/images/jpeg-home.jpg'
-    //                   , contentType: 'image/jpeg'
-    //                   };
-    var urlMedia = { url : 'http://192.168.0.11:8000/screen.png'
-                   , contentType: 'image/png' 
-                   }
-
     device.play(urlMedia, 0, function () {
         console.log('Playing in your chromecast')
 
         device.getStatus(function(err, status) { console.log(status) })
-        // setTimeout(function () {
-        //     //Pause the video
-        //     device.pause(function () {
-        //         console.log('Paused')
-        //     })
-        // }, 20000)
-
-        // setTimeout(function () {
-        //     //Stop video
-        //     device.stop(function () {
-        //         console.log('Stopped')
-        //     })
-        // }, 30000)
-
-        // setTimeout(function () {
-        //     //Close the streaming
-        //     device.close(function () {
-        //         console.log('Closed')
-        //     })
-        // }, 40000)
+        setTimeout(function() { update(device); }, 1000)
     })
+}
+
+browser.on('deviceOn', function (device) {
+   update(device)
 })
--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 FEED_URL=http://127.0.0.1:8090/feed-pi.ffm
-ffmpeg -f x11grab -s 1024x768 -i :0.0 -c:v mpeg4 -f mp4 -f lavfi -i aevalsrc=0 -c:a libmp3lame $FEED_URL
+# ffmpeg -f x11grab -s 1024x768 -i :0.0 -c:v mpeg4 -f mp4 -f lavfi -i aevalsrc=0 -c:a libmp3lame $FEED_URL
+ffmpeg -f x11grab -s 320x200 -i :0.0 -f lavfi -i aevalsrc=0 -c:a libmp3lame $FEED_URL
